78c4bd3e108e54916365f1e72c7ffb41a8b3fb04
WarhearT <warheart666@yandex.ru>
2018-07-11 12:10:24 +0300

    HW02 + options. Version 2/ *Meal* • убрать *все*, что есть в AbstractBaseEntity
    *InMemoryUserRepositoryImpl*
    • delete, get, getByEmail - остались заглушки
    _переименовать MockUserRepositoryImpl в InMemoryUserRepositoryImpl и *имплементировать*_ Т. е. все методы должны работать.
    • getAll
    > 1.3: предусмотрите случай одинаковых User.name (порядок должен быть зафиксированным).
    т. е. если есть несколько юзеров с одинаковыми name, они всегда должны быть в одинаковом порядке.
    
    *MealRepository*
    • getAll - возвращает сортированный список, Collection не подходит, возвращай List
    • delete - должен вернуть boolean (удалили/не удалили)
    
    *MealRepository, MealService*
    • лучше не совмещать getAll и фильтрацию, а сделать отдельные методы
    
    *InMemoryMealRepositoryImpl*
    • стр. 41, 52 - Idea подсказывает, что meal != null всегда верно, если до него дойдет. Т. к. иначе еще раньше бросится NPE (этого надо избежать)
    • isUsersAreEqual - мне кажется, только усложняет. Проще и понятнее - meal.getUserId() == userId
    • getAll - дублируется код в ветках. Общее можно вынести в один приватный метод.
    
    *MealServiceImpl*
    • для проверки результата и выброса исключения используй методы из ValidationUtil (см. UserServiceImpl)
    • update - не бросает исключение
    • стр. 55 - старайся избегать явного приведения типов
    
    *MealRestController*
    • getAll(DateTimeFilter dateTimeFilter) - название не соответствует
    • не стоит использовать один публичный метод из другого и связывать их. Лучше вынести, если надо, в private метод
    • DEFAULT_CALORIES_PER_DAY - лимит калориев тоже берем из SecurityUtil
    
    *UserServlet*
    • init тут не нужен, он ничего не делает
    
    MealRestController
    •
    > 8.1: Закрывать springContext в сервлете грамотнее всего в HttpServlet.destroy(): если где-то в контексте Spring будет ленивая инициализация, метод-фабрика, не синглтон-scope, то контекст понадобится при работе приложения. У нас такого нет, но делать надо все грамотно.
    • SecurityUtil в сервлете не используй. Мы потом сервлет удалим.
    
    *DateTimeUtil*
    > 4: Проверку isBetweenDate сделать в DateTimeUtil. Попробуйте использовать дженерики и объединить ее с isBetweenTime
    
    *UsersUtil*
    • у юзеров одинаковые id
    
    *Форматирование*
    • пробелы стоят/не стоят как попало. Пользуйся автоформатированием Idea (Ctrl+Alt+L)
    
    • подчищай неиспользуемые импорты
    
    > п. 5: включить классы еды в контекст Spring (добавить аннотации) и вызвать из SpringMain любой метод MealRestController (проверить что Spring все корректно заинжектил)

--- src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
@@ -3,20 +3,23 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class UsersUtil {
 
-    public static List<User> getUsers(){
+    public static List<User> getUsers() {
         List<User> users = new ArrayList<>();
         Set<Role> admins = new HashSet<>();
         admins.add(Role.ROLE_ADMIN);
 
-        users.add(new User(0,"FirstUsername","firstEmail@mail.jv","adminpass",2000, true, admins));
-        users.add(new User(1,"SecondUsername","secEmail@mail.jv","admin2pass",3000, true, admins));
-        users.add(new User(1,"SUser","secEmail@mail3.jv","admin2pass",3000, false, admins));
-        users.add(new User(1,"name4","secEmail@mail2.jv","admin2pass",3000, true, admins));
-        users.add(new User(1,"1name","secEmail@mail1.jv","pass",3000, false, admins));
+        users.add(new User(0, "FirstUsername", "firstEmail@mail.jv", "adminpass", 2000, true, admins));
+        users.add(new User(1, "SecondUsername", "secEmail@mail.jv", "admin2pass", 3000, true, admins));
+        users.add(new User(2, "SUser", "secEmail@mail3.jv", "admin2pass", 3000, false, admins));
+        users.add(new User(3, "name4", "secEmail@mail2.jv", "admin2pass", 3000, true, admins));
+        users.add(new User(4, "1name", "secEmail@mail1.jv", "pass", 3000, false, admins));
 
         return users;
     }
